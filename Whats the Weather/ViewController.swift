5d416474097a365c941e37a29ac54460cfa6294d
alanrmendez <alan.mendez07@gmail.com>
2021-01-29 13:13:10 -0500

    Initial Commit

---
+++ Whats the Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift.
+//  Whats the Weather
+//
+//  Created by Alan Mendez on 1/29/21.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
